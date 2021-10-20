a7877468be1e7ca94cf38d18865187d042fd9465
jonathan <jonjonnjonjonn@gmail.com>
2021-10-19 21:25:40 -0500

    added functional counter if click outside of target

--- moving box/script.js
+++ moving box/script.js
@@ -1,14 +1,30 @@
 // const container = document.getElementById(`container`)
 // let box = document.querySelector(`.box`)
-
+const container = document.getElementById(`container`)
+const box = document.createElement(`div`)
 const scoreDisplay = document.querySelector(`#score`)
-let score = 0
+
+let score = 1
 scoreDisplay.textContent = score
 
 
+container.addEventListener(`mousedown`, resetScore)
+
+function resetScore(e) {
+    if (e.offsetX != 0)
+    if (this.id === `container`) {
+    score = 0
+    scoreDisplay.textContent = score
+    }
+}
 
 
-function deleteAddNew() {
+
+function deleteAddNew(e) {
+    console.log(e)
+    score++
+    scoreDisplay.textContent = score
+
     const box = document.querySelector(`.box`)
     box.remove()
     addBox()
@@ -20,15 +36,16 @@ function addBox() {
     const container = document.getElementById(`container`)
     const box = document.createElement(`div`)
     box.classList.add(`box`)
-    
+
     let x = Math.floor(Math.random() * 750)
     let y = Math.floor(Math.random() * 450)
 
     box.style.transform = `translate(${x}px, ${y}px)`
     box.style.backgroundColor = randomColor()
     container.style.backgroundColor = `grey`
+    
     container.append(box)
-    box.addEventListener(`click`, deleteAddNew)
+    box.addEventListener(`mousedown`, deleteAddNew)
 
 
 
